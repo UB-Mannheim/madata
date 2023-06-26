344a8e005be8278fb774ad59425d64f562b22309
shigapov <57352291+shigapov@users.noreply.github.com>
2023-06-26 15:57:17 +0200

    update to v0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="madata",
-    version='0.1.0',
+    version='0.2.0',
     author="Renat Shigapov",
     license="MIT",
     description="A tool for syncing the dataset-metadata between MADATA and Wikidata",
